4e1a2822a7a67c5f9196dd47756b7a14d5a5c694
ellijayne <ellijayne.m.willard@gmail.com>
2018-07-15 21:09:13 +1000

    threw error as was missing one , after :new in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
 root :to => 'pages#home'
     resources :users, :only => [:new, :create, :index]
-    resources :albums, :only => [:index, :show :new, :create]
+    resources :albums, :only => [:index, :show, :new, :create]
     resources :artists, :only => [:new, :create]
     #TODO add index too later!
     resources :pressings, :only => [:new, :create]
